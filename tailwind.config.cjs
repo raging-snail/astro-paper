cc7a336a42c09d588c1c1ff81f44fef0c91ff29f
raging-snail <70616272+raging-snail@users.noreply.github.com>
2024-11-21 09:08:22 +0800

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -63,18 +63,43 @@ module.exports = {
         mono: ["IBM Plex Mono", "monospace"],
       },
 
-      typography: {
+      typography: ({ theme }) => ({
         DEFAULT: {
           css: {
-            pre: {
-              color: false,
+            a: {
+              color: theme('colors.primary.500'),
+              '&:hover': {
+                color: `${theme('colors.primary.600')}`,
+              },
+              code: { color: theme('colors.primary.400') },
+            },
+            'h1,h2': {
+              fontWeight: '700',
+              letterSpacing: theme('letterSpacing.tight'),
+            },
+            h3: {
+              fontWeight: '600',
             },
             code: {
-              color: false,
+              color: theme('colors.indigo.500'),
             },
           },
         },
-      },
+        invert: {
+          css: {
+            a: {
+              color: theme('colors.primary.500'),
+              '&:hover': {
+                color: `${theme('colors.primary.400')}`,
+              },
+              code: { color: theme('colors.primary.400') },
+            },
+            'h1,h2,h3,h4,h5,h6': {
+              color: theme('colors.gray.100'),
+            },
+          },
+        },
+      }),
     },
   },
   plugins: [require("@tailwindcss/typography")],
